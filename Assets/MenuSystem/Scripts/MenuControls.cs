832ea5ece80266819a08e75975a5758c763d4489
OwenDorey <123426282+OwenDorey@users.noreply.github.com>
2023-02-21 13:38:22 +0000

    Initial commit

---
+++ Assets/MenuSystem/Scripts/MenuControls.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour
+{
+    [SerializeField] string playScene = "PlayerSandboxScene";
+    [SerializeField] string mainMenuScene = "StartScene";
+
+    public void StartGame()
+    {
+        Cursor.visible = false;
+        SceneManager.LoadScene(playScene);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT GAME");
+        Application.Quit();
+    }
+}
